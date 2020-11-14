d10fa289f337571734e174cc7869a54f6275f5ad
mao.oosawa2 <maoosawa0514@icloud.com>
2020-11-14 14:49:37 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Mao Osawa on 14/11/2020.
+//  Copyright © 2020 mao.osawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
